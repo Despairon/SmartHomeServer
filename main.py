34e02b0c3b32a1f9d6a87d21804f8b7a9eb3913a
Despairon <maxdespairon@gmail.com>
2019-08-24 15:58:22 +0300

    Fixed port argument type

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from smartHomeServer import SmartHomeServer
 def parseArguments():
 	parser = argparse.ArgumentParser(description="Runs Smart Home Server which serves both devices and client applications via REST API.")
 	
-	parser.add_argument("--host", help="Host name to be used by the server. Don\'t specify this argument to use default value (localhost)")
-	parser.add_argument("--port", help="Host port to be used by the server. Don\'t specify this argument to use default value (8000)")
+	parser.add_argument("--host", type=str, help="Host name to be used by the server. Don\'t specify this argument to use default value (localhost)")
+	parser.add_argument("--port", type=int, help="Host port to be used by the server. Don\'t specify this argument to use default value (8000)")
 
 	return parser.parse_args()
 
